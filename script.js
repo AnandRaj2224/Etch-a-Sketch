9ac58146594a5c2f038d807a0fd0bd9944c040bf
Anand RAj <anandraj6783@gmail.com>
2024-03-31 13:27:08 +0530

    created input area consist of  ainput box a start and reset button

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const heading = document.createElement("div");
 heading.textContent = "Etch-a-Sketch";
 heading.classList = "heading";
 container.appendChild(heading);
+
+// created a div for the input area.
+const inputArea = document.createElement("div");
+inputArea.classList = "inputArea";
+//created a input box for size of grid.
+const input = document.createElement("input");
+input.setAttribute('type','number');
+inputArea.appendChild(input);
+// created a start button to start the grid.
+const startButton = document.createElement("button");
+startButton.textContent = "start";
+startButton.classList = "button";
+inputArea.appendChild(startButton);
+// created a reset button for reseting the grid to default.
+const resetButton = document.createElement("button");
+resetButton.textContent = "reset";
+resetButton.classList = "button";
+inputArea.appendChild(resetButton);
+
