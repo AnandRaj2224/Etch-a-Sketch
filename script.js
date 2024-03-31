2e73cb19e9975f72387bfa68f3d78c6a191936a5
Anand RAj <anandraj6783@gmail.com>
2024-03-31 15:28:26 +0530

    created the game area and game logic

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const inputArea = document.createElement("div");
 inputArea.classList = "inputArea";
 //created a input box for size of grid.
 const input = document.createElement("input");
+input.classList = "input";
 input.setAttribute('type','number');
 inputArea.appendChild(input);
 // created a start button to start the grid.
@@ -24,3 +25,31 @@ resetButton.classList = "button";
 inputArea.appendChild(resetButton);
 container.appendChild(inputArea);
 
+//created a div for area to play the game.
+const gameArea = document.createElement("div");
+gameArea.classList = "gameArea";
+container.appendChild(gameArea);
+startButton.addEventListener('click',startGrid);
+
+//function for starting the grid.
+function startGrid (){
+  const gameArea = document.querySelector(".gameArea");
+  const input = document.querySelector(".input");
+  const value = input.value;
+  for(let i =0;i<value;i++)
+  {
+    gridRow();
+  }
+}
+// function to create a row of divs.
+function gridRow (){
+  const gameArea = document.querySelector(".gameArea");
+  const input = document.querySelector(".input");
+  const value = input.value;
+  for(let i =0;i<value;i++)
+  {
+    const gridELements = document.createElement("div");
+    gridELements.classList = "gridElements";
+    gameArea.appendChild(gridELements);
+  }
+}
