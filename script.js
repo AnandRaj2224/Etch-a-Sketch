aff50898b29ab9ae2a56d79d3e3478199c46e0ac
Anand RAj <anandraj6783@gmail.com>
2024-03-31 12:54:45 +0530

    added a class name for heading

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const container = document.querySelector(".container");
 // created a div for the heading.
 const heading = document.createElement("div");
 heading.textContent = "Etch-a-Sketch";
+heading.classList = "heading";
 container.appendChild(heading);
